a13c3b50f29627078b6c16a789daadc8036444c7
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-05-31 14:29:32 -0700

    Create Car Insurance App
    Customers can enter their information, and then an administrator will be able to see what the quote is for their insurance.

---
+++ CarInsuranceApp/CarInsuranceApp/Controllers/AdminController.cs
@@ -0,0 +1,71 @@
+﻿using CarInsuranceApp.Models;
+using CarInsuranceApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsuranceApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (CarInsuranceEntities db = new CarInsuranceEntities())
+            {
+                var customers = (from c in db.Customers
+                                where c.Quote == null
+                                select c).ToList();
+                var customerVMs = new List<CustomerVM>();
+                foreach (var customer in customers)
+                {
+                    double quote = 50;
+                    var currentYear = DateTime.Now.Year;
+                    var age = currentYear - Convert.ToDateTime(customer.DateOfBirth).Year;
+                    if (age < 18)
+                    {
+                        quote += 100;
+                    }
+                    else if (age < 25 || age > 100)
+                    {
+                        quote += 25;
+                    }
+                    if (customer.CarYear < 2000 || customer.CarYear > 2015)
+                    {
+                        quote += 25;
+                    }
+                    if (customer.CarMake == "Porsche")
+                    {
+                        quote += 25;
+                        if (customer.CarModel == "911 Carrera")
+                        {
+                            quote += 25;
+                        }
+                    }
+                    quote += 10 * (double)customer.SpeedingTickets;
+                    if (customer.DUI == true)
+                    {
+                        quote *= 1.25;
+                    }
+                    if (customer.Coverage == "full")
+                    {
+                        quote *= 1.5;
+                    }
+                    customer.Quote = (decimal)quote;
+
+
+                    var customerVM = new CustomerVM();
+                    customerVM.FirstName = customer.FirstName;
+                    customerVM.LastName = customer.LastName;
+                    customerVM.EmailAddress = customer.EmailAddress;
+                    customerVM.Quote = (decimal)customer.Quote;
+
+                    customerVMs.Add(customerVM);
+                }
+                return View(customerVMs);
+            }
+        }
+    }
+}
---
+++ CarInsuranceApp/CarInsuranceApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using CarInsuranceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsuranceApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetQuote(string firstName, string lastName, string emailAddress, string birthday, bool DUI, string speedingTickets, string carYear, string carMake, string carModel, string coverage)
+        {
+            using (CarInsuranceEntities db = new CarInsuranceEntities())
+            {
+                var customer = new Customer();
+                customer.FirstName = firstName;
+                customer.LastName = lastName;
+                customer.EmailAddress = emailAddress;
+                customer.DateOfBirth = Convert.ToDateTime(birthday);
+                customer.DUI = DUI;
+                customer.SpeedingTickets = Convert.ToInt32(speedingTickets);
+                customer.CarYear = Convert.ToInt32(carYear);
+                customer.CarMake = carMake;
+                customer.CarModel = carModel;
+                customer.Coverage = coverage;
+
+                db.Customers.Add(customer);
+                db.SaveChanges();
+            }
+            return View("CustomerQuote");
+        }
+    }
+}
