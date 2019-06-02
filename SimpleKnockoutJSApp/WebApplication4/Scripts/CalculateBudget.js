0ebf59ce565ea93954d96b2fb9652538337ba844
Tony Smith <46821595+adavidsmith5@users.noreply.github.com>
2019-06-02 11:32:46 -0700

    Create budget app with knockoutjs
    This app just does simple arithmetic to calculate a budget based on user input. It is just to practice using Knockout JS.

---
+++ SimpleKnockoutJSApp/WebApplication4/Scripts/CalculateBudget.js
@@ -0,0 +1,12 @@
+﻿var vm = function (income, rent, water, electric, other) {
+    this.income = ko.observable(income),
+        this.rent = ko.observable(rent),
+        this.water = ko.observable(water),
+        this.electric = ko.observable(electric),
+        this.other = ko.observable(other),
+        this.budget = ko.pureComputed(function () {
+            return this.income() - this.rent() - this.water() - this.electric() - this.other()
+        }, this);
+};
+
+ko.applyBindings(new vm(0,0,0,0,0));
